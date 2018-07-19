95667ba643febf1d51651a10f969309b1b3336ca
Alex Roman <elpitbull6989@gmail.com>
2018-07-19 04:43:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    self.genres.each { |genre| genre_count[genre] = self.genres.count}
+    @@genres.each { |genre| genre_count[genre] = self.genres.count}
     genre_count
   end
 
